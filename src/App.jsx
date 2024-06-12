4f3af272dece2e80b258920de2b10827ca936804
abel-otegbola <abel15655@gmail.com>
2024-06-12 23:40:23 +0100

    feat: add dashboard page into app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import Home from './pages/home/home'
 import Topbar from './components/topbar/topbar'
+import Dashboard from './pages/dashboard'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <Topbar />
       <Routes>
         <Route path={"/"} exact element={<Home />} />
+        <Route path={"/dashboard/*"} exact element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   )
