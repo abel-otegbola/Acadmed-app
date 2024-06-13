b0910679d4fc9e8bdf0d82a2184402a2cbabf85a
abdullahi <moh.abdullahi2003@gmail.com>
2024-06-13 22:05:14 +0100

    working on home page

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,7 +1,16 @@
 export default function Home() {
     return (
-        <div className="">
-            home
+        <div className="flex justify-between items-center w-[1200px] mx-auto my-auto h-[100vh]">
+            <div className="">
+                <h2>
+                    Intelligent Telemedicine Assessment and Simple Schedule planner
+                </h2>
+                <p>We are pioneering the future of healthcare for academias with cutting edge telehealth and telemedicine solutions.</p>
+                <p>AI Technology</p>
+                <a href="#">Get Started</a>
+            </div>
+
+            <img src="/src/assets/hero-image.png" alt="hero image" width="700px" />
         </div>
     )
 } 
