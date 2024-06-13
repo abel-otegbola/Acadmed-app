8bb74204358f7d06a2e2cf20d83be3b6e0ca175f
abdullahi <moh.abdullahi2003@gmail.com>
2024-06-13 22:53:28 +0100

    updated topbar icon

--- src/components/topbar/topbar.jsx
+++ src/components/topbar/topbar.jsx
@@ -1,4 +1,4 @@
-import { FiHome } from 'react-icons/fa'
+import { FiHome, FiDatabase } from 'react-icons/fi'
 
 export default function Topbar() {
     return (
@@ -7,9 +7,9 @@ export default function Topbar() {
             <nav className="">
                 <ul className="flex items-center justify-between gap-[60px]">
 
-                    <li className="flex items-center justify-center gap-2"><img src="/src/assets/Home.png" alt="home image" width="32px" height="32px" className="object-cover" /><p>Home</p></li>
+                    <li className="flex items-center justify-center gap-2"><FiHome /><p>Home</p></li>
                     <li className="flex items-center justify-center gap-2"><img src="/src/assets/about.png" alt="about image" width="32px" height="32px" /><p>About</p></li>
-                    <li className="flex items-center justify-center gap-2"><img src="/src/assets/Dashboard.png" alt="platform image" width="32px" height="32px" /><p>Platform</p></li>
+                    <li className="flex items-center justify-center gap-2"><FiDatabase /><p>Platform</p></li>
                 </ul>
             </nav>
             <img src="/src/assets/profile-pic.png" alt="profile picture" width="42px" height="auto" />
