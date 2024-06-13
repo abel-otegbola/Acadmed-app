13b996d5b457d50c98f86d6a80caa1956790626f
abdullahi <moh.abdullahi2003@gmail.com>
2024-06-13 21:44:06 +0100

    worked on topbar

--- src/components/topbar/topbar.jsx
+++ src/components/topbar/topbar.jsx
@@ -1,7 +1,16 @@
 export default function Topbar() {
     return (
-        <div className="">
-            Topbar
-        </div>
+        <header className="flex justify-between items-center py-3 ">
+            <h1 className="text-[#807185] text-[18px]">Acad <span className="text-[#AC42C7]">Med</span></h1>
+            <nav className="">
+                <ul className="flex items-center justify-between gap-[60px]">
+                    <li className="flex items-center flex-col justify-center gap-1"><img src="/src/assets/Home.png" alt="home image" width="32px" height="32px" className="object-cover" /><p>Home</p></li>
+                    <li className="flex items-center flex-col justify-center gap-2"><img src="/src/assets/about.png" alt="about image" width="32px" height="32px" /><p>About</p></li>
+                    <li className="flex items-center flex-col justify-center gap-2"><img src="/src/assets/Dashboard.png" alt="platform image" width="32px" height="32px" /><p>Platform</p></li>
+                </ul>
+            </nav>
+            <img src="/src/assets/profile-pic.png" alt="profile picture" width="42px" height="auto" />
+
+        </header>
     )
 } 
